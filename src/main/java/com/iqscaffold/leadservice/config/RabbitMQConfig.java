37710b871c66938ad4848b35be8d03f1fd25d03e
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-02-12 19:34:06 +0200

    feat(config): add RabbitAdmin bean to RabbitMQ configuration

--- src/main/java/com/iqscaffold/leadservice/config/RabbitMQConfig.java
+++ src/main/java/com/iqscaffold/leadservice/config/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -128,6 +129,17 @@ public class RabbitMQConfig {
   @Bean
   public MessageConverter jsonMessageConverter() {
     return new Jackson2JsonMessageConverter();
+  }
+
+  /**
+   * Creates a RabbitAdmin for auto-declaring queues, exchanges, and bindings.
+   *
+   * @param connectionFactory RabbitMQ connection factory
+   * @return Configured RabbitAdmin
+   */
+  @Bean
+  public RabbitAdmin rabbitAdmin(final ConnectionFactory connectionFactory) {
+    return new RabbitAdmin(connectionFactory);
   }
 
   /**
